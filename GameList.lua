b00bb6cadf15c559bb707b33306b1ceeb6180ef9
ranklee26-glitch <ranklee26@gmail.com>
2026-09-13 15:33:41 +0700

    Remove old URL for Grow a Garden game

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,5 @@
 local Games = {
     -- Grow a Garden (Daftarkan semua varian ID-nya)
-    [7436755782] = "https://raw.githubusercontent.com/ranklee26-glitch/zylohub/main/games/GrowAGarden.lua",
     [126884695634066] = "https://raw.githubusercontent.com/ranklee26-glitch/zylohub/main/games/GrowAGarden.lua",
 }
 

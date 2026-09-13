b544e9713f17fbfeff7b444fc066889d8cc18168
ranklee26-glitch <ranklee26@gmail.com>
2026-09-13 15:19:55 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,7 @@
 local Games = {
-    -- Grow a Garden
+    -- Grow a Garden (ID Utama & ID Sub-Server Terbaru)
     [7436755782] = "https://raw.githubusercontent.com/ranklee26-glitch/zylohub/main/games/GrowAGarden.lua",
+    [126884695634066] = "https://raw.githubusercontent.com/ranklee26-glitch/zylohub/main/games/GrowAGarden.lua",
 }
 
 return Games

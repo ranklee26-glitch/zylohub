3213a5eeaa79118cc99121218b9865b521e41ea7
ranklee26-glitch <ranklee26@gmail.com>
2026-09-13 14:26:44 +0700

    Initialize Settings.lua with version and Discord link

---
+++ Settings.lua
@@ -0,0 +1,7 @@
+local _env = getgenv and getgenv() or {}
+
+local Ver = "Version: 1.0.0"
+local Dis = "https://discord.com/channels/724526453030387733/724526454397993012"
+
+_env.Discord = Dis
+_env.Version = Ver
